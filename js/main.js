f70b7674a9e08de5bbe1568205764105bcadf348
kumelkalen <free.your.mind.fc@gmail.com>
2017-06-21 01:02:24 -0400

    Creamos un ciclo en el que primero se guarden las claves y sus valores asociados, y después ponemos esos datos en nuestro html para mostrarlos.

--- js/main.js
+++ js/main.js
@@ -15,7 +15,22 @@ function saveComment () {
 
 function display (){
 	var contComment = document.getElementById("ale");
+	
+	for(var i=0;i<localStorage.length;i++){
+		var clave = localStorage.key(i);
+		var valor = localStorage.getItem(clave);
 
+		//Nombre
+		var h = document.createElement("h3");
+		var textName = document.createTextNode(clave);
+		h.appendChild(textName);
+		contComment.appendChild(h);
+		//Comentario
+		var p = document.createElement("p");
+		var textComment = document.createTextNode(valor);
+		p.appendChild(textComment);
+		contComment.appendChild(p);
+	}
 }
 
 
