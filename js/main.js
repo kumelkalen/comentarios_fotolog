e9089a443cae6198eeea0e4468f8e2ae570a0ce5
kumelkalen <free.your.mind.fc@gmail.com>
2017-06-20 23:58:03 -0400

    Guardamos en variables los valores que serán ingresados en los input y los registramos en nuestro local Storage.

--- js/main.js
+++ js/main.js
@@ -3,4 +3,11 @@ function doFirst{
 	var comentar = document.getElementById("add-button");
 	button.addEventListener("click",saveComment,false);
 }
+
+function saveComment{
+	var clave = document.getElementById("clave").value;
+	var valor = document.getElementById("valor").value;
+	localStorage.setItem(clave,valor);
+}
+
 window.addEventListener("load",doFirst,false);
